d350cd18e4421399f721291c64bc7e3c5318fd82
Irabeny <irabeny89@gmail.com>
2025-10-21 00:30:50 +0100

    test: test the fix on single line quoted strings and other edge cases

--- goenvy_test.go
+++ goenvy_test.go
@@ -7,13 +7,36 @@ import (
 
 func TestLoadEnv(t *testing.T) {
 	LoadEnv()
-	testKey := "TEST_KEY"
-	testVal := "test-value"
 
-	envVal := os.Getenv(testKey)
-	if len(envVal) == 0 {
-		t.Errorf("LoadEnv failed to get value for env key %q", testKey)
-	} else if os.Getenv(testKey) != testVal {
-		t.Errorf("LoadEnv value for key %q did not match value %q", testKey, testVal)
+	noSpaceExpect := os.Getenv("NO_SPACE")
+	noSpaceActual := "value1"
+	spacedEntryExpect := os.Getenv("SPACED_ENTRY")
+	spacedEntryActual := "value2"
+	singleQuotedExpect := os.Getenv("SINGLE_QUOTED")
+	singleQuotedActual := "single quote text allowed"
+	doubleQuotedExpect := os.Getenv("DOUBLE_QUOTED")
+	doubleQuotedActual := "double quote text allowed"
+	singleQuotesMultilineExpect := os.Getenv("SINGLE_QUOTES_MULTILINE")
+	singleQuotesMultilineActual := "single quotes first line\nanother line but ensure they are wrapped in quotes\n"
+	doubleQuotesMultilineExpect := os.Getenv("DOUBLE_QUOTES_MULTILINE")
+	doubleQuotesMultilineActual := "double quotes first line\nanother line but ensure they are wrapped in quotes"
+
+	if noSpaceExpect != noSpaceActual {
+		t.Errorf("LoadEnv failed to get environment variable with no space around '=' assignment operator")
+	}
+	if spacedEntryExpect != spacedEntryActual {
+		t.Errorf("LoadEnv failed to get environment variable with space around the '=' assignment operator")
+	}
+	if singleQuotedExpect != singleQuotedActual {
+		t.Errorf("LoadEnv failed to get environment variable with single quotes value")
+	}
+	if doubleQuotedExpect != doubleQuotedActual {
+		t.Errorf("LoadEnv failed to get environment variable with double quotes value")
+	}
+	if singleQuotesMultilineExpect != singleQuotesMultilineActual {
+		t.Errorf("LoadEnv failed to get environment variable with multiline values wrapped in single quotes")
+	}
+	if doubleQuotesMultilineExpect != doubleQuotesMultilineActual {
+		t.Errorf("LoadEnv failed to get environment variable with multiline values wrapped in double quotes")
 	}
 }
