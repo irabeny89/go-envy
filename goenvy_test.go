e20dc09962d5444a36de7389fa028efb620b0130
Irabeny <irabeny89@gmail.com>
2025-10-25 05:58:11 +0100

    test: test LoadEnvPath function and update env files for this purpose

--- goenvy_test.go
+++ goenvy_test.go
@@ -40,3 +40,45 @@ func TestLoadEnv(t *testing.T) {
 		t.Errorf("LoadEnv failed to get environment variable with multiline values wrapped in double quotes")
 	}
 }
+
+func TestLoadEnvPathOverwrite(t *testing.T) {
+	LoadEnv()
+	LoadEnvPath(".env.development")
+
+	noSpaceExpect := os.Getenv("NO_SPACE")
+	noSpaceActual := "anotherfilevalue1"
+	spacedEntryExpect := os.Getenv("SPACED_ENTRY")
+	spacedEntryActual := "anotherfilevalue2"
+	singleQuotedExpect := os.Getenv("SINGLE_QUOTED")
+	singleQuotedActual := "another file single quote text allowed"
+	doubleQuotedExpect := os.Getenv("DOUBLE_QUOTED")
+	doubleQuotedActual := "another file double quote text allowed"
+	singleQuotesMultilineExpect := os.Getenv("SINGLE_QUOTES_MULTILINE")
+	singleQuotesMultilineActual := "another file single quotes first line\nanother line but ensure they are wrapped in quotes\n"
+	doubleQuotesMultilineExpect := os.Getenv("DOUBLE_QUOTES_MULTILINE")
+	doubleQuotesMultilineActual := "another file double quotes first line\nanother line but ensure they are wrapped in quotes"
+	extraExpect := os.Getenv("EXTRA")
+	extraActual := "this value will be appended if not overwritten from next LoadEnvPath"
+
+	if noSpaceExpect != noSpaceActual {
+		t.Error("LoadEnvPath failed to read & overwrite env value with no space around '=' from previous LoadEnv call")
+	}
+	if spacedEntryExpect != spacedEntryActual {
+		t.Error("LoadEnvPath failed to read & overwrite env value with space around '=' from previous LoadEnv call")
+	}
+	if singleQuotedExpect != singleQuotedActual {
+		t.Error("LoadEnvPath failed to read & overwrite env value with single quotes from previous LoadEnv call")
+	}
+	if doubleQuotedExpect != doubleQuotedActual {
+		t.Error("LoadEnvPath failed to read & overwrite env value with double quotes from previous LoadEnv call")
+	}
+	if singleQuotesMultilineExpect != singleQuotesMultilineActual {
+		t.Error("LoadEnvPath failed to read & overwrite env value with multiline single quotes from previous LoadEnv call")
+	}
+	if doubleQuotesMultilineExpect != doubleQuotesMultilineActual {
+		t.Error("LoadEnvPath failed to read & overwrite env value with multiline double quotes from previous LoadEnv call")
+	}
+	if extraExpect != extraActual {
+		t.Error("LoadEnvPath failed to get extra value from previous env file")
+	}
+}
